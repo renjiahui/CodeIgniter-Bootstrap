1da49e7956eefb4f83a1715d8edf7853a7ad134e
Gary Ren <gary.ren@beecrazy.com>
2013-12-18 17:16:54 +0800

    for test

--- application/controllers/frontpage.php
+++ application/controllers/frontpage.php
@@ -8,6 +8,47 @@ class Frontpage extends Main_Controller {
       $this->load->view('include/footer');
 	}
    
+   public function test1()
+   {
+      /*
+       * This breaks normal CodeIgniter convention.
+       * We need to load the views first before we can
+       * execute the unit testing.
+       */
+      
+      
+      $data['tests'] = 'test';
+ 
+      $this->load->view('include/header');
+      $this->load->view('templates/menubar', $data);
+      $this->load->view('templates/login', $data);
+      $this->load->view('include/footer');
+   }
+
+   public function test2()
+   {
+      /*
+       * This breaks normal CodeIgniter convention.
+       * We need to load the views first before we can
+       * execute the unit testing.
+       */
+      $this->load->helper('form');
+      $this->load->helper('string');
+      $this->load->helper('string');
+      $this->load->helper('inflector');
+      
+      $fields = array();
+      $fields[] = array( 'name' => 'first_name', 'type' => 'text', 'default' => 'First Name');
+      $fields[] = array( 'name' => 'password', 'type' => 'password', 'default' => 'First Name');
+      $fields[] = array( 'name' => 'password_confirm', 'type' => 'password', 'default' => 'First Name');
+      
+      $data['fields'] = $fields;
+ 
+      $this->load->view('include/header');
+      $this->load->view('templates/menubar', $data);
+      $this->load->view('templates/form', $data);
+      $this->load->view('include/footer');
+   }
 }
 
 /* End of file frontpage.php */
